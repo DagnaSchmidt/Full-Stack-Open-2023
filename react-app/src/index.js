3349bbb9bc180bbfcac9afcc6f282b133f70f6aa
Dagna Schmidt <dagdagi889@gmail.com>
2023-05-22 11:41:11 +0200

    part 1 react-app

---
+++ react-app/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+// change import to see other projects
+import App from '../../part 1/course information/App'; // course information
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+    <App />
+);
