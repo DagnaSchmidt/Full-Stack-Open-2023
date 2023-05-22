30e2a641d85e55ff708de02ce86c5c7834866ac4
Dagna Schmidt <dagdagi889@gmail.com>
2023-05-22 18:43:23 +0200

    part1 1.7

--- react-app/src/part 1/unicafe/App.js
+++ react-app/src/part 1/unicafe/App.js
@@ -5,14 +5,21 @@ const App = () => {
     const [good, setGood] = useState(0);
     const [neutral, setNeutral] = useState(0);
     const [bad, setBad] = useState(0);
+    const [average, setAverage] = useState(0);
+
+    const all = good + neutral + bad;
+    const positive = (good / all) * 100;
+    const averageProc = average / all;
 
     const handleClick = (text) => {
         if(text === 'good'){
             setGood(good + 1);
+            setAverage(average + 1);
         }else if(text === 'neutral'){
             setNeutral(neutral + 1);
         }else{
             setBad(bad + 1);
+            setAverage(average -1);
         }
     }
   
@@ -25,6 +32,9 @@ const App = () => {
         <p>good {good}</p>
         <p>neutral {neutral}</p>
         <p>bad {bad}</p>
+        <p>all {good + neutral + bad}</p>
+        <p>average {all === 0 ? '0' : averageProc}</p>
+        <p>positive {all === 0 ? '0' : positive} %</p>
       </div>
     )
 }
