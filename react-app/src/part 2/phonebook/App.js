4eb96d657d0e498ff647bd324647ed9c2c263a03
Dagna Schmidt <dagdagi889@gmail.com>
2023-05-23 21:32:20 +0200

    part2 2.9

--- react-app/src/part 2/phonebook/App.js
+++ react-app/src/part 2/phonebook/App.js
@@ -1,9 +1,17 @@
 import { useState } from 'react'
 
 const App = () => {
-  const [persons, setPersons] = useState([{ name: 'Arto Hellas' }]); 
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas', phone: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', phone: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', phone: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', phone: '39-23-6423122', id: 4 }
+  ]);
   const [newName, setNewName] = useState('');
   const [newPhone, setNewPhone] = useState('');
+  const [search, setSearch] = useState('');
+  const searchedPersons = persons.filter((item) => item.name.toLocaleLowerCase().includes(search.toLocaleLowerCase()));
+  console.log(searchedPersons);
 
   const handleSubmit = e => {
     e.preventDefault();
@@ -21,7 +29,8 @@ const App = () => {
             ...persons,
             {
                 name: newName,
-                phone: newPhone
+                phone: newPhone,
+                id: persons.length + 1
         }
         ]);
     }else{
@@ -33,18 +42,19 @@ const App = () => {
     const {value, name} = e.target;
     if(name === 'name'){
         setNewName(value);
-    }else{
+    }else if(name === 'phone'){
         setNewPhone(value);
+    }else{
+        setSearch(value);
     }
   }
-
-  console.log(newName);
-  console.log(newPhone);
-  console.log(persons);
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <h2>Search</h2>
+      <input type='text' name='search' value={search} onChange={handleChange} />
+      <h2>Add new number</h2>
       <form onSubmit={handleSubmit}>
         <div>
             name: <input type='text' name='name' value={newName} onChange={handleChange} />
@@ -57,7 +67,12 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      {persons.map((item) => <p key={item.name}>{item.name} {item.phone && item.phone}</p>)}
+      {
+        search.length > 0 ?
+            searchedPersons.map((item) => <p key={item.name}>{item.name} {item.phone}</p>)
+        :
+            persons.map((item) => <p key={item.name}>{item.name} {item.phone}</p>)
+      }
     </div>
   )
 }
