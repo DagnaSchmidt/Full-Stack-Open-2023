1df1c3c94c67e52be4794e53165663d155b21de4
Dagna Schmidt <dagdagi889@gmail.com>
2023-05-23 17:23:38 +0200

    part 2.3

--- react-app/src/part 2/course-information/components/Total.js
+++ react-app/src/part 2/course-information/components/Total.js
@@ -3,13 +3,16 @@ import React from 'react';
 const Total = ({parts}) => {
   const total = parts.map((item) => item.exercises)
   let amount = 0;
-
-  for(let i = 0; i < total.length; i++){
-    amount = (amount + total[i]);
-  }
+  // for(let i = 0; i < total.length; i++){
+  //   amount = (amount + total[i]);
+  // }
+  const result = total.reduce(
+    (a, c) => a + c,
+    amount
+  );
 
     return (
-      <p>total of {amount} exercises</p>
+      <p>total of {result} exercises</p>
     )
   }
 
