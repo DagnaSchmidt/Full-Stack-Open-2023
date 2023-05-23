25386df76530a5e3dc5365e84829ce628f647da0
Dagna Schmidt <dagdagi889@gmail.com>
2023-05-23 16:44:35 +0200

    part2 course information start code

--- react-app/src/index.js
+++ react-app/src/index.js
@@ -4,7 +4,9 @@ import ReactDOM from 'react-dom/client';
 // change import to see other projects
 // import App from './part 1/course information/App';        // course information
 // import App from './part 1/unicafe/App';                  //unicafe
-import App from './part 1/anecdotes/App';                  //anecdotes
+// import App from './part 1/anecdotes/App';                  //anecdotes
+import App from './part 2/course-information/App';
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
